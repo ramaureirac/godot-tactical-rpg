86be8ef2ade2717c8a88c9e84adc05980c4bc050
Rodrigo Maureira Contreras <ramaureirac@protonmail.com>
2019-07-04 16:24:54 -0400

    new ai pathfinding (nearest ally)

--- scripts/trpg_pawn.gd
+++ scripts/trpg_pawn.gd
@@ -41,10 +41,8 @@ func _ally_act(var tile):
 	self.path = self._gen_path(tile)
 
 func _enemy_act():
-	#if self._ai_get_nearest_ally() != null:
-	#	self.path = self._ai_gen_path_nearest_ally()
-	#else:
-	self.path = self._ai_gen_chase_path()
+	self.available_mvmts = self._gen_all_mvmts()
+	self.path = self._ai_get_path_nearest_ally()
 
 
 # aux functions:
@@ -79,7 +77,7 @@ func _gen_available_mvmts():
 										neighbor.mv_available = true
 										av_mvmts.append(neighbor)
 									process_queue.push_back(neighbor)
-			print("Pawn: they're ", av_mvmts.size(), " tiles availables for move")
+			#print("Pawn: they're ", av_mvmts.size(), " tiles availables for move")
 			return av_mvmts
 	return self.available_mvmts
 
@@ -111,13 +109,13 @@ func _gen_all_mvmts():
 										neighbor.mv_available = true
 										av_mvmts.append(neighbor)
 								process_queue.push_back(neighbor)
-			print("Pawn: they're ", av_mvmts.size(), " tiles availables for move")
+			#print("Pawn: they're ", av_mvmts.size(), " tiles availables for move")
 			return av_mvmts
 	return self.available_mvmts
 
-func _gen_path(var dstny_tile, var ignore_mv_available = false):
+func _gen_path(var dstny_tile, var ignore_not_available = false):
 	if !self.available_mvmts.empty() && self.path.empty():
-		if dstny_tile != null && (dstny_tile.mv_available || ignore_mv_available):
+		if dstny_tile != null && (dstny_tile.mv_available || ignore_not_available):
 			var mv_path = []
 			while dstny_tile.root != null:
 				mv_path.push_front(dstny_tile)
@@ -152,86 +150,27 @@ func _reset_acts():
 
 
 # AI functions:
-func _ai_gen_path_nearest_ally():
-	self.available_mvmts = self._gen_available_mvmts()
-	return self._ai_get_path_nearest_ally()
-
-func _ai_gen_chase_path():
-	self.available_mvmts = self._gen_all_mvmts()
-	return self._ai_get_chase_path()
-
-func _ai_get_nearest_ally(var use_height = true):
-	var nearest_allies = []
-	if use_height:
-		var allies = self.arena.allies
-		for ally in allies:
-			var height = abs(ally.get_translation().y - self.get_translation().y)
-			if  height <= self.jump_height:
-				nearest_allies.append(ally)
-		if nearest_allies.empty():
-			return null
-	else:
-		nearest_allies = self.arena.allies
-	var nearest_ally = nearest_allies.front()
-	for ally in nearest_allies:
-		var from = self.get_translation()
-		var distance = from.distance_to(ally.get_translation())
-		var nearest_distance = from.distance_to(nearest_ally.get_translation())
-		if distance < nearest_distance:
-			nearest_ally = ally
-	return nearest_ally
-
-func _ai_get_nearest_tile(var pawn):
-	if pawn != null:
-		var nearest_tile = self.available_mvmts.front()
-		for tile in self.available_mvmts:
-			var from = tile.get_translation()
-			var nearest_distance = nearest_tile.get_translation().distance_to(pawn.get_translation())
-			var distance = from.distance_to(pawn.get_translation())
-			if distance < nearest_distance:
-				nearest_tile = tile
-		return nearest_tile
-	return null
-
 func _ai_get_path_nearest_ally():
-	if self.path.empty() && !self.available_mvmts.empty():
-		var ally = self._ai_get_nearest_ally()
-		var tile = self._ai_get_nearest_tile(ally)
-		if tile == null:
-			return self._gen_path(self.curr_tile)
-		return self._gen_path(tile)
-	return self.path
-
-func _ai_get_nearest_neighbor(var tile):
-	var neighbors = []
-	for neighbor in tile.neighbors:
-		if neighbor.curr_pawn == null:
-			var height = abs(neighbor.get_translation().y - tile.get_translation().y)
-			if height <= self.jump_height:
-				neighbors.append(neighbor)
-	if !neighbors.empty():
-		var nearest = neighbors.front()
-		for neighbor in neighbors:
-			var nearest_distance =  self.get_translation().distance_to(nearest.get_translation())
-			var distance = self.get_translation().distance_to(neighbor.get_translation())
-			if distance < nearest_distance:
-				nearest = neighbor
-		return nearest
-	return tile.neighbors.front()
-
-func _ai_get_chase_path():
 	if !self.available_mvmts.empty() && self.path.empty():
-		var ally = self._ai_get_nearest_ally(false)
-		var tile = self._ai_get_nearest_neighbor(ally.curr_tile)
-		var mv_path = self._gen_path(tile, true)
+		var mv_path = []
+		var distance = 99999
 		for ally in self.arena.allies:
-			tile = self._ai_get_nearest_neighbor(ally.curr_tile)
-			var tmp_path = self._gen_path(tile)
-			if tmp_path.size() > 1 && tmp_path.size() < mv_path.size():
-				mv_path = tmp_path
-		while (mv_path.size() > self.move_range + 1):
+			for neighbor in ally.curr_tile.neighbors:
+				var height = abs(neighbor.get_translation().y - ally.curr_tile.get_translation().y)
+				if height <= 1:
+					if neighbor.get_translation() == self.get_translation():
+						return [self.curr_tile]
+					if neighbor.root != null:
+						if neighbor.curr_pawn == null:
+							var tmp_path = self._gen_path(neighbor, true)
+							if tmp_path.size() < distance:
+								distance = tmp_path.size()
+								mv_path = tmp_path
+		while !mv_path.empty() && mv_path.back().curr_pawn != null:
 			mv_path.pop_back()
-		while mv_path.back().curr_pawn != null && mv_path.size() > 1:
+		while mv_path.size() > self.move_range + 1:
 			mv_path.pop_back()
+		if mv_path.empty():
+			mv_path.push_front(self.curr_tile)
 		return mv_path
 	return self.path
