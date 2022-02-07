7d20f3873de79852bbcbd7134b9269f646a1065b
Rodrigo Maureira Contreras <rodmaureirac@protonmail.com>
2022-02-06 23:32:07 -0300

    refactor player and enemies logic + enemy chase improved

--- gd/arena.gd
+++ gd/arena.gd
@@ -1,5 +1,30 @@
 extends Spatial
 
+func find_nearest_tile_reachable(var tile):
+	while !tile.reachable:
+		tile = tile.root
+	return tile
+
+func find_nearest_tile_neighbor(var from, var to):
+	to.inv_weight = 0
+	var pq = [to]
+	var chk = []
+	var tile = from
+	while !pq.empty():
+		var t = pq.pop_front()
+		if t.root or t == from:
+			if t.inv_weight < tile.inv_weight:
+				tile = t
+			elif t.inv_weight == tile.inv_weight and t.weight < tile.weight:
+				tile = t
+		else:
+			for n in t.neighbors:
+				if !(n in chk) and !(n in pq):
+					n.inv_weight = t.inv_weight + 1
+					pq.append(n)
+		chk.append(t)
+	return tile
+
 func _gen_movement_tree(var tile):
 	"""
 	generate a movement tree linking each tile of the arena with his parent.
@@ -9,32 +34,32 @@ func _gen_movement_tree(var tile):
 	while !pq.empty():
 		var t = pq.pop_front()
 		for n in t.neighbors:
-			if n.root == null and n != tile and !n.taken:
-				n.root = t
-				n.weight = t.weight + 1
-				pq.push_back(n)
+			if n != tile:
+				if n.root == null and !n.taken:
+					n.root = t
+					n.weight = t.weight + 1
+					pq.push_back(n)
 
-func mark_available_movements(var from, var d=10):
+func mark_available_movements(var from, var d):
 	"""
-	mark which tiles are reachable acording
+	mark which tiles are reachable according
 	to 'd' parameter. First it link all possible destinations.
 	"""
 	if from == null: return
 	self._gen_movement_tree(from)
 	for t in $Tiles.get_children():
-		if t == from or t.weight <= d and t.weight > 0:
+		if t == from or t.weight <= d and t.weight > 0 and !t.taken:
 			t.reachable = true
 
-func gen_path(var t):
+func gen_path(var tile):
 	"""
-	generate path to 't' only if 't' was tagged as reachable
-	by the 'mark_available_movements' method.
+	generate path to 'tile'
 	"""
 	var path = []
-	while t != null and t.reachable:
-		path.push_front(t)
-		t.path_gen = true
-		t = t.root
+	while tile != null:
+		path.push_front(tile)
+		tile.path_gen = true
+		tile = tile.root
 	return path
 
 func reset():
---
+++ gd/player.gd
@@ -0,0 +1,49 @@
+extends Node
+
+"""
+Script for handling player actions, like select and move a pawn
+camera movement and rotation, etc.
+"""
+
+var t_camera = null
+var arena = null
+var curr_pawn = null
+var pawns = []
+
+func configure(var my_camera, var my_arena):
+	self.t_camera = my_camera
+	self.arena = my_arena
+	self.pawns = self.get_children()
+
+func camera_rotation():
+	if Input.is_action_just_pressed("cam_rotate_right"):
+		self.t_camera.y_rot += 90
+	if Input.is_action_just_pressed("cam_rotate_left"):
+		self.t_camera.y_rot -= 90
+
+func _select_pawn_for_movement():
+	self.curr_pawn = self.t_camera.select_pawn()
+	if !self.curr_pawn: return
+	if !self.curr_pawn.can_act(): self.curr_pawn = null
+	if !(self.curr_pawn in self.pawns): self.curr_pawn = null
+	if self.curr_pawn != null and self.curr_pawn.can_act():
+		var t = self.curr_pawn.get_tile()
+		var d = self.curr_pawn.distance
+		self.t_camera.set_target(t)
+		self.arena.mark_available_movements(t, d)
+
+func _select_tile_for_movement():
+	var t = self.t_camera.select_tile()
+	if t == null: return
+	if t.reachable:
+		self.t_camera.set_target(t)
+		self.curr_pawn.path_stack = self.arena.gen_path(t)
+
+func ally_move_pawn():
+	if Input.is_action_just_pressed("ui_accept"):
+		if self.curr_pawn == null:
+			self._select_pawn_for_movement()
+		elif self.curr_pawn.path_stack.empty():
+			self._select_tile_for_movement()
+		return self.curr_pawn
+			
