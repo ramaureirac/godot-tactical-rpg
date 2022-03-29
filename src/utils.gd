8e882fdb3546bb860ff84b89088e9066a46f8b39
Rodrigo Maureira Contreras <ramaureirac@protonmail.com>
2022-03-29 14:38:30 -0300

    Added new sprites

--- src/utils.gd
+++ src/utils.gd
@@ -1,11 +1,14 @@
 const TILE_SRC = "res://src/tile.gd"
 
-enum PAWN_CLASSES {Knight, Archer, Chemist, Cleric}
+enum PAWN_CLASSES {Knight, Archer, Chemist, Cleric, Skeleton, SkeletonCPT, SkeletonMage}
 enum PAWN_STRATEGIES {Tank, Flank, Support}
 const KNIGHT_SPRITE = "res://assets/sprites/characters/chr_pawn_knight.png"
 const ARCHER_SPRITE = "res://assets/sprites/characters/chr_pawn_archer.png"
 const CHEMIST_SPRITE = "res://assets/sprites/characters/chr_pawn_chemist.png"
 const CLERIC_SPRITE = "res://assets/sprites/characters/chr_pawn_mage.png"
+const SKELETON_CPT_SPRITE = "res://assets/sprites/characters/chr_pawn_skeleton_cpt.png"
+const SKELETON_SPRITE = "res://assets/sprites/characters/chr_pawn_skeleton.png"
+const SKELETON_MAGE_SPRITE = "res://assets/sprites/characters/chr_pawn_skeleton_mage.png"
 
 
 static func convert_tiles_into_static_bodies(var tiles_obj):
@@ -55,27 +58,36 @@ static func create_material(var color, var texture=null):
 
 
 static func get_pawn_sprite(var pawn_class):
-    match pawn_class:
-        0: return load(KNIGHT_SPRITE)
-        1: return load(ARCHER_SPRITE)
-        2: return load(CHEMIST_SPRITE)
-        3: return load(CLERIC_SPRITE)
+	match pawn_class:
+		0: return load(KNIGHT_SPRITE)
+		1: return load(ARCHER_SPRITE)
+		2: return load(CHEMIST_SPRITE)
+		3: return load(CLERIC_SPRITE)
+		4: return load(SKELETON_SPRITE)
+		5: return load(SKELETON_CPT_SPRITE)
+		6: return load(SKELETON_MAGE_SPRITE)
     
 
 static func get_pawn_move_radious(var pawn_class):
-    match pawn_class:
-        0: return 3
-        1: return 5
-        2: return 4
-        3: return 4
+	match pawn_class:
+		0: return 3
+		1: return 5
+		2: return 4
+		3: return 4
+		4: return 5
+		5: return 3
+		6: return 4
 
 
 static func get_pawn_jump_height(var pawn_class):
-    match pawn_class:
-        0: return 0.5
-        1: return 3
-        2: return 1
-        3: return 1
+	match pawn_class:
+		0: return 0.5
+		1: return 3
+		2: return 1
+		3: return 1
+		4: return 3
+		5: return 0.5
+		6: return 1
 
 
 static func get_pawn_attack_radious(var pawn_class):
@@ -84,6 +96,9 @@ static func get_pawn_attack_radious(var pawn_class):
 		1: return 6
 		2: return 3
 		3: return 3
+		4: return 6
+		5: return 1
+		6: return 3
 
 
 static func get_pawn_attack_power(var pawn_class):
@@ -92,6 +107,9 @@ static func get_pawn_attack_power(var pawn_class):
 		1: return 10
 		2: return 12
 		3: return 12
+		4: return 10
+		5: return 20
+		6: return 12
 
 
 static func get_pawn_health(var pawn_class):
@@ -100,6 +118,9 @@ static func get_pawn_health(var pawn_class):
 		1: return 35
 		2: return 30
 		3: return 25
+		4: return 35
+		5: return 50
+		6: return 30
 
 
 static func vector_remove_y(var vector):
